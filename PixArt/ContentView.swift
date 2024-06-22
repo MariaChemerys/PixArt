9d3d79411a7477ec21445a4c5af13bb212f18562
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-06-22 12:40:14 +0200

    Initial Commit

---
+++ PixArt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PixArt
+//
+//  Created by Mariia Chemerys on 22.06.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PixArt/PixArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixArtApp.swift
+//  PixArt
+//
+//  Created by Mariia Chemerys on 22.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct PixArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
