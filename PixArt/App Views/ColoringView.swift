35cf17c01c7cabaf63a63ffb8e86d57fb44898bc
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-06-27 21:23:01 +0200

    Add the numbered rectangles of pictures

--- PixArt/App Views/ColoringView.swift
+++ PixArt/App Views/ColoringView.swift
@@ -5,6 +5,7 @@
 //  Created by Mariia Chemerys on 25.06.2024.
 //
 
+
 import SwiftUI
 
 struct ColoringView: View {
@@ -13,8 +14,8 @@ struct ColoringView: View {
     @Binding var chosenPicture: Picture
     
     @StateObject var coloringState = ColoringState()
+    
     @State private var isScaled = false
-    @State private var scaledButtonIndex: Int? = 0
     @State private var chosenColorNumber: Int = 0
     @State private var selectedRowIndex: Int?
     @State private var selectedColumnIndex: Int?
@@ -39,18 +40,54 @@ struct ColoringView: View {
             AnimatedBackgroundView(firstColor: chosenPicture.firstDominantColor, secondColor: chosenPicture.secondDominantColor)
             
             VStack {
-                Button(action: {
-                    withAnimation(.easeInOut(duration: 1.0)){
-                        self.currentScreen = .pickPicture
+                HStack{
+                    Button(action: {
+                        withAnimation(.easeInOut(duration: 1.0)){
+                            self.currentScreen = .pickPicture
+                        }
+                        softHaptic()
+                    })
+                    {
+                        Image(systemName: "arrow.left")
+                            .font(.largeTitle)
+                            .foregroundColor(.black)
                     }
-                    softHaptic()
-                })
-                {
-                    Image(systemName: "arrow.left")
-                        .font(.largeTitle)
-                        .foregroundColor(.black)
+                    Text(chosenPicture.name)
                 }
-                Text(chosenPicture.name)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading)
+                
+                Spacer()
+                
+                VStack(spacing: 1){
+                    ForEach(0..<boardHeight){
+                        rowIndex in
+                        HStack(spacing: 1){
+                            ForEach(0..<boardWidth){
+                                columnIndex in
+                                
+                                Text("\(numberedSquaresIntegers[rowIndex][columnIndex])")
+                                    .frame(width: 17, height: 22)
+                                    .foregroundColor(
+                                        squareColors[rowIndex][columnIndex] == .softGray ? .white : squareColors[rowIndex][columnIndex]
+                                    )
+                                    .font(.system(size: 14))
+                                    .background(
+                                        Rectangle()
+                                            .foregroundColor(squareColors[rowIndex][columnIndex]))
+                                    .onTapGesture {
+                                        selectedRowIndex = rowIndex
+                                        selectedColumnIndex = columnIndex
+                                        selectedNumber = numberedSquaresIntegers[rowIndex][columnIndex]
+                                        if (selectedRowIndex == rowIndex && selectedColumnIndex == columnIndex) && chosenColorNumber == selectedNumber {
+                                            let newColor = Color(coloringState.getColor(squareNumber: selectedNumber!))
+                                            squareColors[rowIndex][columnIndex] = newColor
+                                        }
+                                    }
+                            }
+                        }
+                    }
+                }
                 
                 Spacer()
                 
@@ -70,23 +107,16 @@ struct ColoringView: View {
                                     .background(coloringState.getColor(squareNumber: number))
                                     .clipShape(Circle())
                                     .onTapGesture {
-                                        scaledButtonIndex = number
                                         chosenColorNumber = number
                                         softHaptic()
                                     }
-                                    .scaleEffect(scaledButtonIndex == number ? 1.5 : 1.2)
-                                    
+                                    .scaleEffect(chosenColorNumber == number ? 1.5 : 1.2)
                             }
-                            .contentShape(Circle())
-                            .accessibility(addTraits: .isButton)
                         }
                     }
                     .padding(25)
-                  
                 }
-              
                 .background(.thinMaterial)
-                
                 
             }
         }
