d3ed95ea1f47484301f54de5e9138d5b4cf07457
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-07-22 21:41:36 +0200

    Write comments for the ContentView

--- PixArt/App Views/ContentView.swift
+++ PixArt/App Views/ContentView.swift
@@ -7,13 +7,14 @@
 
 import SwiftUI
 
+// Main view for the application
 struct ContentView: View {
     
     @State var currentScreen: Screen = .pickPicture
     @State var chosenPicture: Picture = Picture(name: "panda", category: "Animals", firstDominantColor: .cyan, secondDominantColor: .green)
     
     var body: some View {
-        
+        // Switch statement to navigate between views
         switch currentScreen {
         case .pickPicture:
             PickPictureView(currentScreen: $currentScreen, chosenPicture: $chosenPicture)
