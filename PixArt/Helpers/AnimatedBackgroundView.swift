bd0a1a048e6f6d8f956fb1a26a577db5ab9c8ea1
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-07-23 14:44:27 +0200

    Write comments for the AnimatedBackgroundView

--- PixArt/Helpers/AnimatedBackgroundView.swift
+++ PixArt/Helpers/AnimatedBackgroundView.swift
@@ -7,13 +7,18 @@
 
 import SwiftUI
 
+// The AnimatedBackgroundView struct represents a view with an animated gradient background.
 struct AnimatedBackgroundView: View {
     
+    // State variable to manage the animation state of the gradient.
     @State private var animateGradient: Bool = false
+    
+    // Variables to store the first and second colors of the gradient.
     var firstColor: Color
     var secondColor: Color
     
     var body: some View {
+        // Linear gradient starting from the top leading to the bottom trailing.
         LinearGradient(colors: [firstColor, secondColor], startPoint: .topLeading, endPoint: .bottomTrailing)
             .hueRotation(.degrees(animateGradient ? 45 : 0))
             .onAppear {
