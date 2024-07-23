6085146026ed60329f6a8c6e51493ce501afcada
Maria Chemerys <129984077+MariaChemerys@users.noreply.github.com>
2024-07-23 14:42:15 +0200

    Write comments for the ColoringView

--- PixArt/App Views/ColoringView.swift
+++ PixArt/App Views/ColoringView.swift
@@ -8,6 +8,7 @@
 
 import SwiftUI
 
+// The ColoringView struct represents the view for coloring a selected picture.
 struct ColoringView: View {
     
     @Binding var currentScreen: Screen
@@ -21,33 +22,37 @@ struct ColoringView: View {
     @State private var selectedColumnIndex: Int?
     @State private var selectedNumber: Int?
     
-    // Introduce a state variable to store colors for each rectangle
+    // An array to store colors for each square in the grid.
     @State private var squareColors: [[Color]] = Array(repeating: Array(repeating: .softGray, count: 21), count: 20)
     
     var body: some View {
         
+        // Read the lines from the file corresponding to the chosen picture.
         let numberedSquaresStrings = coloringState.readLinesFromFile(fileName: "\(chosenPicture.name) numbered grid")
         
-        let boardWidth = coloringState.getBoardWidth(arrayOfNumberedSquaresStrings: numberedSquaresStrings)
-        
-        let boardHeight = coloringState.getBoardHeight(arrayOfNumberedSquaresStrings: numberedSquaresStrings)
-        
+        // Convert the lines to a 2D integer array representing the grid.
         let numberedSquaresIntegers = coloringState.squareNumbersLinesTo2DIntArray(lines: numberedSquaresStrings)
         
+        // Get the unique numbers present in the grid which represent different colors.
         let numbersOfPresentColors = coloringState.getPresentColorsNumbers(squareNumbersInt2DArray: numberedSquaresIntegers)
         
+        // Get the dimensions of the board.
+        let boardWidth = coloringState.getBoardWidth(arrayOfNumberedSquaresStrings: numberedSquaresStrings)
+        let boardHeight = coloringState.getBoardHeight(arrayOfNumberedSquaresStrings: numberedSquaresStrings)
+        
         ZStack {
+            // Animated background view with the dominant colors of the chosen picture.
             AnimatedBackgroundView(firstColor: chosenPicture.firstDominantColor, secondColor: chosenPicture.secondDominantColor)
             
             VStack {
-                HStack{
+                // Back button to navigate to the previous screen.
+                HStack {
                     Button(action: {
-                        withAnimation(.easeInOut(duration: 1.0)){
+                        withAnimation(.easeInOut(duration: 1.0)) {
                             self.currentScreen = .pickPicture
                         }
                         softHaptic()
-                    })
-                    {
+                    }) {
                         Image(systemName: "arrow.left")
                             .font(.largeTitle)
                             .foregroundColor(.black)
@@ -58,13 +63,11 @@ struct ColoringView: View {
                 
                 Spacer()
                 
-                VStack(spacing: 1){
-                    ForEach(0..<boardHeight){
-                        rowIndex in
-                        HStack(spacing: 1){
-                            ForEach(0..<boardWidth){
-                                columnIndex in
-                                
+                // Grid view to display the numbered squares and color them
+                VStack(spacing: 1) {
+                    ForEach(0..<boardHeight) { rowIndex in
+                        HStack(spacing: 1) {
+                            ForEach(0..<boardWidth) { columnIndex in
                                 Text("\(numberedSquaresIntegers[rowIndex][columnIndex])")
                                     .frame(width: 17, height: 22)
                                     .foregroundColor(
@@ -73,8 +76,10 @@ struct ColoringView: View {
                                     .font(.system(size: 14))
                                     .background(
                                         Rectangle()
-                                            .foregroundColor(squareColors[rowIndex][columnIndex]))
+                                            .foregroundColor(squareColors[rowIndex][columnIndex])
+                                    )
                                     .onTapGesture {
+                                        // Handle tap gesture to color the square.
                                         selectedRowIndex = rowIndex
                                         selectedColumnIndex = columnIndex
                                         selectedNumber = numberedSquaresIntegers[rowIndex][columnIndex]
@@ -92,9 +97,8 @@ struct ColoringView: View {
                 
                 // Color Picker
                 ScrollView(.horizontal) {
-                    HStack(spacing: 33.0){
-                        ForEach(numbersOfPresentColors, id:\.self){
-                            number in
+                    HStack(spacing: 33.0) {
+                        ForEach(numbersOfPresentColors, id: \.self) { number in
                             Button(action: {
                                 withAnimation {
                                     self.isScaled.toggle()
